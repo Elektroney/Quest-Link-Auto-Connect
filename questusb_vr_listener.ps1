c6e17fda2c4bf0007cd3ea56d7816034e2f90213
Leon Kraim <offleon.kraim@gmail.com>
2024-07-11 02:38:55 +0200

    Add files via upload

--- questusb_vr_listener.ps1
+++ questusb_vr_listener.ps1
@@ -1,18 +1,12 @@
-
+# ADB command to be executed
 $adbCommand = 'adb shell am start -S com.oculus.xrstreamingclient/.MainActivity'
 
-function OnUsbInserted {
-    echo "USB device inserted. Connecting QuestLink..."
+# Register WMI event to listen for USB device insertions
+Register-WmiEvent -Query "SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'" -Action {
     Start-Process -FilePath "adb" -ArgumentList "shell am start -S com.oculus.xrstreamingclient/.MainActivity"
 }
 
-Write-Output "Registering USB insertion event..."
-$usbEvent = Register-WmiEvent -Query "SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'" -Action {
-    OnUsbInserted
-}
-
-
-Write-Output "Listening for USB insertions. Press Ctrl+C to exit."
+# Keep the script running to listen for events
 while ($true) {
     Start-Sleep -Seconds 5
 }
